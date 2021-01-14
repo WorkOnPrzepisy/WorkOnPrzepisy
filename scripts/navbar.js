1b461aedb8d571cce388cf9d746663d734b6ed39
Damiant94 <damian.tulacz@gmail.com>
2021-01-14 02:02:47 +0100

    added animation to toggle menu on mobile, styled hamburger icon, small fixes and styling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,11 @@
 const toggleButton = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".nav-links");
 const loginLogout = document.querySelector(".login-logout");
+const nav = document.querySelector(".nav");
 
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     loginLogout.classList.toggle('active');
+    nav.classList.toggle('active');
+    toggleButton.classList.toggle('clicked');
 });
