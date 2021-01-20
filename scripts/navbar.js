5e415d80e695eed57e8e109d9a36b3c3a72ab144
Damiant94 <damian.tulacz@gmail.com>
2021-01-20 22:56:50 +0100

    added const to functions

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -8,20 +8,25 @@ toggleButton.onclick = () => {
     toggleButton.classList.toggle('clicked');
 }
 
+
+
+// temporary functions for fake log in and log out
+
 const loginButton = document.querySelector(".login-signup a");
 const logoutButton = document.querySelector(".logout a");
 
 const forLogged = document.querySelectorAll(".for-logged");
 const forUnlogged = document.querySelector(".for-unlogged");
 
-handleLogin = () => {
+
+const handleLogin = () => {
     for (const node of forLogged) {
         node.style.display = "flex";
     }
     forUnlogged.style.display = "none";
 };
 
-handleLogout = () => {
+const handleLogout = () => {
     for (const node of forLogged) {
         node.style.display = "none";
     }
