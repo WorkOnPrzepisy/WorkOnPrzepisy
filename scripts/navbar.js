be4b5093922f1eb90dd4a783b8eb42b2353978d8
Damiant94 <damian.tulacz@gmail.com>
2021-01-20 23:40:17 +0100

    code refactor

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,8 @@
 const toggleButton = document.querySelector(".toggle-button");
-const navLinks = document.querySelector(".nav-links");
 const nav = document.querySelector(".nav");
 
 toggleButton.onclick = () => {
-    navLinks.classList.toggle('active');
     nav.classList.toggle('active');
-    toggleButton.classList.toggle('clicked');
 }
 
 
@@ -18,27 +15,16 @@ const logoutButton = document.querySelector(".logout a");
 const forLogged = document.querySelectorAll(".for-logged");
 const forUnlogged = document.querySelector(".for-unlogged");
 
-
-const handleLogin = () => {
-    for (const node of forLogged) {
-        node.style.display = "flex";
-    }
-    forUnlogged.style.display = "none";
+const handleLogin = (forLoggedDisplay, forUnloggedDisplay) => {
+    for (const node of forLogged) node.style.display = forLoggedDisplay;
+    forUnlogged.style.display = forUnloggedDisplay;
 };
-
-const handleLogout = () => {
-    for (const node of forLogged) {
-        node.style.display = "none";
-    }
-    forUnlogged.style.display = "flex";
-};
-
 
 loginButton.onclick = () => {
-    handleLogin();
+    handleLogin("flex", "none");
 }
 
 logoutButton.onclick = () => {
-    handleLogout();
+    handleLogin("none", "flex");
 }
 
