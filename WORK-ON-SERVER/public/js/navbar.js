3a4456ec8b4d236080f8e2d57867c316b53db8dc
Damiant94 <damian.tulacz@gmail.com>
2021-01-29 20:32:42 +0100

    fixed issues

--- WORK-ON-SERVER/public/js/navbar.js
+++ WORK-ON-SERVER/public/js/navbar.js
@@ -1,8 +1,30 @@
 const toggleButton = document.querySelector(".toggle-button");
-const navLinks = document.querySelector(".nav-links");
-const loginLogout = document.querySelector(".login-logout");
+const nav = document.querySelector(".nav");
 
-toggleButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    loginLogout.classList.toggle('active');
-});
+toggleButton.onclick = () => {
+    nav.classList.toggle('active');
+}
+
+
+
+// temporary functions for fake log in and log out
+
+const loginButton = document.querySelector(".login-signup a");
+const logoutButton = document.querySelector(".logout a");
+
+const forLogged = document.querySelectorAll(".for-logged");
+const forUnlogged = document.querySelector(".for-unlogged");
+
+const handleLogin = (forLoggedDisplay, forUnloggedDisplay) => {
+    for (const node of forLogged) node.style.display = forLoggedDisplay;
+    forUnlogged.style.display = forUnloggedDisplay;
+};
+
+loginButton.onclick = () => {
+    handleLogin("flex", "none");
+}
+
+logoutButton.onclick = () => {
+    handleLogin("none", "flex");
+}
+
