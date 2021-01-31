f30e5e3b15cac47ffbb7703c61e03bb285d4074d
Kamila Liskiewicz <kamila.liskiewicz12@gmail.com>
2021-01-31 21:53:18 +0100

    com

--- WORK-ON-SERVER/db.js
+++ WORK-ON-SERVER/db.js
@@ -1,28 +1,26 @@
 // const dbConnect =require("dotenv").config();
 const { connect } = require("mongoose");
 
-// const uri = 'mongodb+srv://damiant94:dtHasSQnmbgPzNsU@cluster0.haa8v.mongodb.net/<dbname>?retryWrites=true&w=majority'
-const uri = 'mongodb://127.0.0.1:27017/'
+const uri = 'mongodb+srv://damiant94:dtHasSQnmbgPzNsU@cluster0.haa8v.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    //const uri = 'mongodb://127.0.0.1:27017/'
 
 
-const dbConnection = async () => {
-  try {
-    const db = await connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: ""
-    });
+const dbConnection = async() => {
+    try {
+        const db = await connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: ""
+        });
 
-    if (db) {
-      console.log("mongoDB connected");
+        if (db) {
+            console.log("mongoDB connected");
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 module.exports = {
-  dbConnection,
-}; 
-
-
+    dbConnection,
+};
